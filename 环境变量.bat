b8a40b3d1db150b5ecf9ed052e959a8cc768b766
CaesarDemonCC <hsbj_email@163.com>
2016-09-02 17:05:11 +0800

    Add files via upload
    上传源代码、环境、编译脚本

---
+++ 环境变量.bat
@@ -0,0 +1,6 @@
+@echo off
+echo 添加C#环境变量
+
+setx path "%path%;C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+
+pause>nul
---
+++ 编译exe.bat
@@ -0,0 +1,3 @@
+csc /r:ICSharpCode.SharpZipLib.dll;NPOI.dll;NPOI.OOXML.dll;NPOI.OpenXml4Net.dll;NPOI.OpenXmlFormats.dll *.cs
+
+pause
